7695e513b00033419a1bc24ab18e3b670c264f3d
MagdaOnaindia <156606073+MagdaOnaindia@users.noreply.github.com>
2025-10-29 17:48:02 +0100

    Change to net 8.0

--- src/TFM.Dashboard/Program.cs
+++ src/TFM.Dashboard/Program.cs
@@ -1,16 +1,12 @@
+using Microsoft.Extensions.DependencyInjection; // Add this using directive
+using Microsoft.OpenApi.Models; // Add this using directive
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+
 
 app.UseHttpsRedirection();
 
@@ -21,7 +17,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
